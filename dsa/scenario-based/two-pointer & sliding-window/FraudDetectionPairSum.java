14c391a0448373db028155010973f1dff013fd20
Aditya Kumar Sharma <adityasharmaagra2003@gmail.com>
2026-07-28 15:59:08 +0530

    Practice of Two pointer and Sliding window is done

---
+++ dsa/scenario-based/two-pointer & sliding-window/FraudDetectionPairSum.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class FraudDetectionPairSum {
+    public static int[] findPairSum(int[] transactions, int target) {
+        int left = 0;
+        int right = transactions.length - 1;
+        while (left < right) {
+            int sum = transactions[left] + transactions[right];
+            if (sum == target) {
+                return new int[]{transactions[left], transactions[right]};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        int[] transactions = {10000, 15000, 20000, 24999, 30000};
+        System.out.println(Arrays.toString(findPairSum(transactions, 49999)));
+    }
+}
